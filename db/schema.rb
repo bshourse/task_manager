3fe4e148207482fe7ab33843dc5b2fd28c76c33c
Dmitry <agapov.box@gmail.com>
2021-01-28 02:09:54 +0300

    Added models associations

---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_27_225049) do
+
+  create_table "projects", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "project_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+    t.string "task_name"
+    t.integer "performer_id"
+    t.date "due_date"
+    t.time "implementation_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = []
+5.times do |i|
+  u = User.create(first_name: "user_#{i+1}", last_name: "user_#{i+1}_last_name", email: "test_#{i+1}@test.com", password_digest: BCrypt::Password.create("12#{i}"))
+  users << u.id
+end
+
+projects = []
+3.times do |i|
+  p = Project.create(user_id: users.sample, project_name: "Project_#{i+1}")
+  projects << p.id
+end
+
+20.times do |i|
+  Task.create(project_id: projects.sample, user_id: users.sample, task_name: "Тестовая задача #{i+1}", description: "Здесь должно быть описание #{i+1}-ой задачи", performer_id: users.sample, due_date: Date.today + rand(15), implementation_time: "")
+end
